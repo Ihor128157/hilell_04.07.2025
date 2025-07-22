f8bc1c3731a4650cb8bef89fde8ab3dc46b3277a
LOQ <verovka.ihor@gmail.com>
2025-07-22 08:03:09 +0300

    22 july 2025 commit

---
+++ lesson2/homework2_2.py
@@ -0,0 +1,5 @@
+number = int(input("Введіть п’ятизначне число: "))
+
+reversed_number = (number % 10) * 10000 + ((number // 10) % 10) * 1000 + ((number // 100) % 10) * 100 + ((number // 1000) % 10) * 10 + (number // 10000)
+
+print("Число задом наперед:", reversed_number)
